b65dafc02fef7ad6ba6a535da99bc316e62c3592
jsking216 <jking@onecause.com>
2020-04-27 19:47:38 -0400

    finish up skills, remove portfolio

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -25,6 +25,31 @@ class Resume extends Component {
     return (
       <section id="resume">
 
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1><span>Work</span></h1>
+        </div>
+
+        <div className="nine columns main-col">
+        {work}
+        </div>
+      </div>
+
+      <div className="row skill">
+         <div className="three columns header-col">
+            <h1><span>Skills</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <p>{skillmessage}
+            </p>
+				<div className="bars">
+				   <ul className="skills">
+					  {skills}
+					</ul>
+				</div>
+			</div>
+
       <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
@@ -38,41 +63,8 @@ class Resume extends Component {
             </div>
          </div>
       </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
-        </div>
     </div>
-
-
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
-    );
+  </section>);
   }
 }
 
