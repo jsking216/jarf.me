6b1dbf13a416cd66691cdfbc35413c5d6bb5b821
Joshua King <jarf1337@gmail.com>
2020-10-17 21:29:45 -0400

    improve header nav contrast

--- components/Header.js
+++ components/Header.js
@@ -23,10 +23,10 @@ class Header extends Component {
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
          <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <li className="navlink"><a className="smoothscroll" href="#home">Home</a></li>
+            <li className="navlink"><a className="smoothscroll" href="#about">About</a></li>
+	         <li className="navlink"><a className="smoothscroll" href="#resume">Resume</a></li>
+            <li className="navlink"><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
       </nav>
