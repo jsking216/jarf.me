bfc006c5a88c2562a3846a6f09524c7cf94e3d7c
Joshua King <jarf1337@gmail.com>
2020-10-25 12:24:15 -0400

    sitemap script

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 module.exports = withPlugins([
-  [optimizedImages, {}],
-]);
+  [optimizedImages, {}],], {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  }
+});
