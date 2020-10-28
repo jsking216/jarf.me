92cd87e5a5fd46822be2398c925c11c6b08dd91d
Joshua King <jarf1337@gmail.com>
2020-10-28 16:20:06 -0400

    nvm css optimization

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const withCss = require("@zeit/next-css");
-const withPurgeCss = require("next-purgecss")
 
 module.exports = withPlugins([
-  [optimizedImages, {}],
-  withCss,
-  [withPurgeCss, {
-    purgeCssPaths: [
-      "pages/**/*",
-      "components/**/*",
-      "public/**/*", // also scan other-components folder
-    ]
-  }]]);
+  [optimizedImages, {}],]);
